934447ce82d0bf4700cfd63de3a5587e00013cef
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-05-14 15:54:51 -0400

    update

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 
 # Create your views here.
 
@@ -7,3 +6,8 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+class AboutPageView(TemplateView): # new
+    template_name = 'about.html'
+    
+    
+    

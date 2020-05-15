0fce479e67568fc0dd5a29865d34be06cfeb2592
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-05-15 12:19:10 -0400

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import SimpleTestCase
+
+class SimpleTests(SimpleTestCase):
+    
+    def test_home_page_status_code(self):
+        
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_about_page_status_code(self):
+        
+        response = self.clent.get('/about/')
+        self.assertEqual(response.status_code, 200)
+    
+    
